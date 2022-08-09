3815aff9e165cd9ed86ce28e7e8f4101c8cfe63c
mdasif8393 <md.asif.8393@gmail.com>
2022-08-09 15:49:32 +0600

    add for loop

---
+++ odd-even-while-loop.js
@@ -0,0 +1,14 @@
+// let start = 1;
+
+// while(start<=10){
+//     if(start%2!=0){
+//         console.log(start);
+//     }
+//     start++;
+// }
+
+let number = 0;
+while(number <=20){
+    console.log(number);
+    number = number + 2;
+}
