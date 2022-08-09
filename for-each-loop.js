484600796a2f218cbd0fa7b38c9f3f04b667fb1f
mdasif8393 <md.asif.8393@gmail.com>
2022-08-09 16:11:31 +0600

    complete

---
+++ for-each-loop.js
@@ -0,0 +1,11 @@
+// const array1 = ['a', 'b', 'c'];
+// array1.forEach(element => console.log(element));
+
+// const names =['Abul', 'Kabul', 'Jabul', 'Mobul'];
+// names.forEach(name => console.log(name));
+
+
+const names =['Abul', 'Kabul', 'Jabul', 'Mobul'];
+for(let i =0; i<names.length; i++){
+    console.log(names[i]);
+}
